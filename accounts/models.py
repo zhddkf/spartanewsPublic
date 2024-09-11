a64b2fbad1f219a44e89c63e71dfb96608435a55
HeureuseD <da4260@daum.net>
2024-09-11 21:32:23 +0900

    feat: soft delete구현

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,12 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     birth_date = models.DateField()
     gender = models.CharField(max_length=10, null=True, blank=True)
+
+    def soft_delete(self):
+        self.is_active = False
+        self.save()
+        return True
+
+    def delete(self, using=None, keep_parents=False):
+        self.soft_delete()
+        return True
