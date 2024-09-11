4b3fb30d095d119a9aca88045d0f9dfab4dd9e1c
HeureuseD <da4260@daum.net>
2024-09-11 21:22:43 +0900

    feat: logout, 회원탈퇴 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView 
-from .views import SignupAPIView 
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import SignupAPIView, LogoutAPIView, DeleteAPIView
 urlpatterns = [
-    path("signup/", SignupAPIView.as_view(), name='signup'),
+    path("signup/", SignupAPIView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("delete/", DeleteAPIView.as_view(), name="user_delete"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,48 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, PasswordCheckSerializer
 
 User = get_user_model()
 
-class SignupAPIView(APIView):
+class SignupAPIView(APIView): # 회원가입
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAPIView(APIView): # 로그아웃
+    def post(self, request):
+        refresh = request.data.get("refresh")
+        if refresh is None:
+            return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            token = RefreshToken(refresh)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteAPIView(APIView): # 회원탈퇴
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+        serializer = PasswordCheckSerializer(data=request.data)
+        if serializer.is_valid():
+            password = serializer.validated_data['password']
+            user = authenticate(username=request.user.username, password=password)
+            if user is not None:
+                user.soft_delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
